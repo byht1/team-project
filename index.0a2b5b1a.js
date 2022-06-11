6e90ce71239611d9a180ce4b545e12f5199a6cf2
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-11 14:26:12 +0000

    Deploying to gh-pages from @ byht1/team-project@284883457eb3062baa9d4128af33c6ea0c943f6e 🚀

---
+++ index.0a2b5b1a.js
@@ -0,0 +1,2 @@
+$((function(){$(".box-slick").slick({arrows:!1,dots:!0,infinite:!1,slidesToShow:1,slidesToScroll:1,adaptiveHeight:!0})}));
+//# sourceMappingURL=index.0a2b5b1a.js.map
--- index.5de92064.js
+++
@@ -1,2 +0,0 @@
-$((function(){$(".box-slick").slick({dots:!0,infinite:!1,slidesToShow:1,slidesToScroll:1})}));
-//# sourceMappingURL=index.5de92064.js.map
