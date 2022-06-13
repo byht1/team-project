24fe5cd971b10b987c6ebad4c9fad1c61c18e61b
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-13 18:34:50 +0000

    Deploying to gh-pages from @ byht1/team-project@7a5afd983bbb5cfebbb100d620d719f800e8cf54 🚀

---
+++ index.399431fd.js
@@ -0,0 +1,2 @@
+!function(){var t={btn:document.querySelector(".js-open-menu"),textOne:document.querySelector(".js-text-one"),textTwo:document.querySelector(".js-text-two")},e=0;t.btn.addEventListener("click",(function(){0===e?(t.btn.style.transform="rotateY(180deg)",e+=1):(t.btn.style.transform="rotateY(0)",e-=1),t.textOne.classList.toggle("імя класа"),t.textTwo.classList.toggle("імя класа")}))}();
+//# sourceMappingURL=index.399431fd.js.map
