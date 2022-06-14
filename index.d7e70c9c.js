45f7b12db0f3a4a0843927eec18fe7597631b6fe
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-14 19:36:50 +0000

    Deploying to gh-pages from @ byht1/team-project@044b190934f380ef6efe508ed3c54f95f3c081d6 🚀

--- index.d7e70c9c.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden-about")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.d7e70c9c.js.map
---
+++ index.e4039c3f.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden-about"),o.body.classList.toggle("hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.e4039c3f.js.map
