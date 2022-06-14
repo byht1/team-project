408924b822d2fb61ef6c3be060f558dddb5db3a2
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-14 18:35:33 +0000

    Deploying to gh-pages from @ byht1/team-project@4aeb49395e89615a9f6ef84cf4d830d7a065fd36 🚀

---
+++ index.d7e70c9c.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden-about")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.d7e70c9c.js.map
