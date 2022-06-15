b439011b4c6c3f48988eaad716f0cf9601ad0a0d
byht1 <vitalikbyht1@gmail.com>
2022-06-15 16:09:29 +0300

    esc and закриттня по натисканню на бекдроп

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,12 +7,52 @@
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtnOne.addEventListener('click', toggleModal);
+  let a = 0;
+
+  refs.openModalBtnOne.addEventListener('click', toggleModalTwo);
   refs.openModalBtnTwo.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModalEnd);
+
+  document.addEventListener('keyup', () => {
+    const isEscape = event.code === 'Escape';
+    const isActiv = document.querySelector('.avtiv-modal');
+    if (!isActiv) {
+      return;
+    }
+    if (!isEscape) {
+      return;
+    }
+    if (a === 1) {
+      refs.modal.classList.toggle('is-hidden');
+      refs.modal.classList.toggle('avtiv-modal');
+      refs.body.classList.toggle('hidden');
+      return;
+    }
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('avtiv-modal');
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('avtiv-modal');
     refs.body.classList.toggle('hidden');
+
+    a = 1;
+  }
+  function toggleModalTwo() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('avtiv-modal');
+    a = 0;
+  }
+
+  function toggleModalEnd() {
+    if (a === 1) {
+      refs.modal.classList.toggle('is-hidden');
+      refs.modal.classList.toggle('avtiv-modal');
+      refs.body.classList.toggle('hidden');
+      return;
+    }
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('avtiv-modal');
   }
 })();
