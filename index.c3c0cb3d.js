da95ba79f01800113a4d9d425b5d6a9b50def2d7
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-15 13:10:49 +0000

    Deploying to gh-pages from @ byht1/team-project@e39155013cdf7c4d7999f5d7b78bf63695455cd9 🚀

--- index.c3c0cb3d.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden-about"),o.body.classList.toggle("hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.c3c0cb3d.js.map
